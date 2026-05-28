a7324971a4ba31ee2c533a6507178e25cf53785b
b33456386-cmd <b33456386@gmail.com>
2026-05-28 23:31:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
-from telethon import TelegramClient
-import re
+import requests
 import os
 
-api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("BOT_TOKEN")
 target_channel = os.getenv("TARGET_CHANNEL")
 
-client = TelegramClient('session', api_id, api_hash)
+message = "🔥 Bot Connected Successfully"
 
-source_channels = [
-    'freewireguard',
-]
+url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 
-vmess_pattern = r'vmess://[^\s]+'
-vless_pattern = r'vless://[^\s]+'
+data = {
+    "chat_id": target_channel,
+    "text": message
+}
 
-async def main():
+requests.post(url, data=data)
 
-    await client.start()
-
-    for channel in source_channels:
-
-        async for message in client.iter_messages(channel, limit=20):
-
-            if not message.text:
-                continue
-
-            configs = []
-
-            configs += re.findall(vmess_pattern, message.text)
-            configs += re.findall(vless_pattern, message.text)
-
-            for config in configs:
-
-                text = f"🔥 New Config\n\n{config}"
-
-                await client.send_message(target_channel, text)
-
-                print("Sent")
-
-with client:
-    client.loop.run_until_complete(main())
+print("Message Sent")
